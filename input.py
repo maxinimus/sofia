f3992261225d1716443639a0d5a8298cbbba5702
Maxim Lavrenko <eternalbirch@gmail.com>
2023-07-22 15:17:29 +0300

    Update input.py

--- input.py
+++ input.py
@@ -1,12 +1,11 @@
 # Purpose: Input file for the project
-# Simply a place to store the input data for the project
+# Simply a place to store the input data for the query
 
 # For examlpe, if you want to store the code for the project, you can paste it here, and 
-# then use a [code] in the prompt to use it, such as prompt='Here is the [code] for the project'
-# the [code] will be autimatically replaced with the code below. You can do the same for any other
-# input you want to store, such as the readme, or the data, or anything else you want to store,
-# and don't forget to import it in the main.py file and tweak the code
+# then use [code] in the prompt to use it, such as prompt='Here is the [code] for the project'
+# the [code] will be autimatically replaced with the code below before sending it to the chat bot. You can do the same for any other
+# input you want to store, such as a readme file, or anything else you want to store
+# and don't forget to import it in the main.py file and tweak the code, if you want to create another variable (code is already imported and works)
 
 code = """
 """
-
